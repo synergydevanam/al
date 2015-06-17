5ed27aa9a176f6e1975d050980c0198adcaee6f6
anam <anamuliut@gmail.com>
2015-06-17 23:01:08 +0600

    dsfsdaf

---
+++ update/Default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class update_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Redirect("https://docs.google.com/spreadsheets/d/1H8uDKq1AjlA1xBDM6GyEl34w56zb0Qq8D9IED7aVYew/edit?usp=sharing");
+    }
+}
